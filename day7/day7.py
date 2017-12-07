c29a1aa5f66b17db95a76950fecf3366f6d60676
swehner <Sebastian.Wehner@bioinfsys.uni-giessen.de>
2017-12-07 18:08:47 +0100

    day 7 part 1 solved

--- day7/day7.py
+++ day7/day7.py
@@ -32,54 +32,6 @@ def find_name_of_first(tree):
     # below here should never be reached
 
 
-def find_the_unbalanced_program(tree, root, curr_unbalanced="", curr_weight=0):
-    """calc the sum of all candidates"""
-    candidates = [ n for n,v in tree.items() if v["children"] ]
-
-    # calculate the overall sum of each stack and save it
-    for c in candidates:
-        ws = [ tree[w]['size'] for w in tree[c]['children'] ]
-        # print(c, ws, sum(ws)+tree[c]['size'])
-        tree[c]['sumsize'] = tree[c]['size'] + sum(ws)
-
-    # go depth first through the tree starting with the root
-    bal = __traverse__(tree, root)
-    print(bal, tree[bal])
-
-    # find the parent of the balanced bal
-    for c in candidates:
-        if bal in tree[c]['children']:
-            unbal = c
-            break
-    print(c, tree[c])
-
-
-
-def __traverse__(tree, node):
-    # check if sum of balanced stacks
-    stack = [ tree[c]['sumsize'] for c in tree[node]['children'] ]
-
-    if len(set(stack)) == 1:
-        return node
-    else:
-        for ch in tree[node]['children']:
-            return __traverse__(tree, ch)
-
-
-
-
-def __find_unbalanced__(tree, node, unbal="", lst=[]):
-    # base case
-    child_sizes = [ tree[c]['sumsize'] for c in tree[node]['children'] ]
-    if len(set(child_sizes)) <= 1:
-        unbal, lst = node, child_sizes
-    else:
-        for c in tree[node]['children']:
-            __find_unbalanced__(tree, c)
-    return unbal, lst
-
-
-
 if __name__ == "__main__":
 
     filename = "../testing_inputs/tree.txt"
@@ -119,5 +71,3 @@ if __name__ == "__main__":
     first = find_name_of_first(tree)
     print("The name of the first program is: {}".format(first))
 
-    # find wrongly weighted program
-    wrong = find_the_unbalanced_program(tree, first)
